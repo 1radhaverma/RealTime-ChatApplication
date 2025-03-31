66ea904b6d5162d5356f4d937f8bd4ea791d5456
Radha Verma <radhaverma2246@gmail.com>
2025-03-31 21:34:59 +0530

    Backend+Db

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
