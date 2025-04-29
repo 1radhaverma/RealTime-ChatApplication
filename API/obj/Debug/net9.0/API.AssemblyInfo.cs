94342ab680299bf7da8d755de50b5d901d875b01
Radha Verma <radhaverma1099@gmail.com>
2025-04-29 16:14:36 +0530

    ui changes

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ea904b6d5162d5356f4d937f8bd4ea791d5456")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a582ec8602b39246537599f70f0a685a379e00c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
