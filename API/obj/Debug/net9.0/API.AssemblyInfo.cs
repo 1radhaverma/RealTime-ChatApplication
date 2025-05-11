827886d60f71932aa6fb0e3fa7ee841401e9f49b
Radha Verma <radhaverma2246@gmail.com>
2025-05-11 19:11:29 +0530

    Fix chat history display issues and responsiveness

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6336acdcc989bf21e51d1ddf2cbe890245bf9851")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea3c2bb4595486566f29b5494e4a2409b529dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
